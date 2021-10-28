57f90ebfec429226aa66a2ae8d8afb4f63de6b7e
neoncloud <neoncloud@users.noreply.github.com>
2021-10-28 23:04:38 +0800

    Update build_ipas.sh

--- tools/build_ipas.sh
+++ tools/build_ipas.sh
@@ -23,7 +23,7 @@ cp -r "$1" Payload/Fugu14App.app
 zip -r Fugu14_Setup.ipa Payload
 
 # Inject Spotlight
-./pwnify_compiled Payload/Fugu14App.app/Fugu14App "$2/Applications/Spotlight.app/Spotlight"
+./pwnify_compiled Payload/Fugu14App.app/Fugu14App "$2/Spotlight"
 
 # Build second IPA
 cp Fugu14_Setup.ipa Fugu14_Pwn.ipa
